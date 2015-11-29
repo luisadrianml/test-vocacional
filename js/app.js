4561e505e89507f005e4988bcb783ea03cd0d134
Luis Adrian Martinez Lopez <Luis Adrian Martinez Lopez>
2015-11-29 02:00:09 -0400

    Add control for display questions of yes/no

--- js/app.js
+++ js/app.js
@@ -4,8 +4,12 @@ angular.module('testVApp', []);
 angular.module('testVApp')
     .controller("MainCtrl", function ($scope, $http) {
         $scope.buttons = [1 ,0];
-        $scope.currentStep = 0;
-        $scope.moveFoward = 0;
+        $scope.textQuestionSection = [{"yes": "Me gusta", "no":"Me disgusta"}, {"yes": "Si", "no":"No"}];
+        $scope.currentQuestion = 0;
+        var textVars = ["R", "I", "A", "S", "E", "C"];
+        $scope.currentVariable = 0;
+        $scope.currentSection = 0;
+        $scope.sectionOfYes = 0;
         $scope.vars = {
             R: 0,
             I: 0,
@@ -14,26 +18,46 @@ angular.module('testVApp')
             E: 0,
             C: 0
         };
-        $scope.section = null;
+        $scope.sections = null;
 
         $http.get("/data/questions.json").then(function (res) {
             console.info(res);
-            $scope.section = res.data.sections[0];
-            console.info($scope.section);
+            $scope.sections = res.data.sections;
+            console.info($scope.sections);
         });
 
-        $scope.setR = function () {
-            $scope.R += 1;
-        }
-
         $scope.saveClick = function(answer) {
-            $scope.vars.R += answer;
-            $scope.currentStep++;
 
-            if ($scope.currentStep===11) {
-                $scope.moveFoward++;
-                $scope.currentStep = 0;
+            $scope.vars[textVars[$scope.currentVariable]] += answer;
+            $scope.currentQuestion++;
+            if ($scope.currentQuestion===11 && $scope.currentSection < 2) {
+                $scope.currentVariable++;
+                $scope.currentQuestion = 0;
                 console.log("test");
+
+                if ($scope.currentVariable == 6) { // Changing the Section
+                    $scope.currentSection++;
+                    $scope.currentQuestion = 0;
+                    $scope.currentVariable=0;
+                }
+            } else if($scope.currentSection == 2 && $scope.currentQuestion===14) {
+                $scope.currentVariable++;
+                $scope.currentQuestion = 0;
+                console.log("test222");
+
+                if ($scope.currentVariable == 6) { // Changing the Section
+                    // Specify to ask the other questions :autoevaluacion:
+                    console.log("we got here");
+                }
+            } else {
+                console.error("we got baaad!")
             }
+
+
+
+            if ($scope.currentSection == 1) {
+                $scope.sectionOfYes = 1;
+            }
+
         }
     });
